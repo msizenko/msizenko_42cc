fce12347f3eaab1d0c7981597e68c729ff61140b
Marina Sizenko <msizenko@gmail.com>
2012-07-23 14:24:09 +0300

    (ticket: 1) Added test for testing data loaded from fixtures

--- msizenko_42cc/apps/assignment/models.py
+++ msizenko_42cc/apps/assignment/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from annoying.fields import AutoOneToOneField
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = AutoOneToOneField(User, primary_key=True)
+    date_of_birth = models.DateField(blank=True)
+    bio = models.TextField()
+    
+class Contact(models.Model):
+    TYPE_CHOICES = (
+        ('JB', 'jabber'),
+        ('SK', 'skype'),
+        ('TW', 'twitter')
+    )
+    type = models.CharField(max_length=2, choices=TYPE_CHOICES)
+    value = models.CharField(max_length=254)
+    user = models.ForeignKey(UserProfile)
+    
+    class Meta:
+        verbose_name = "contact"
+        
+    def __unicode__(self):
+        return "%s: %s" % (self.get_type_display(), self.value)
+    
+    
+
+    
--- msizenko_42cc/apps/assignment/views.py
+++ msizenko_42cc/apps/assignment/views.py
@@ -5,3 +5,6 @@ def test(request):
     Test page.
     """
     return render_to_response("assignment/test.html")
+    
+def index(request):
+    return render_to_response("assignment/index.html")
