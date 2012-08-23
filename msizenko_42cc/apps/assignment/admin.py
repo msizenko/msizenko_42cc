2266fbce4ce6c5d06e4616793a6c4452cb3ed87d
Marina Sizenko <msizenko@gmail.com>
2012-08-23 11:43:55 +0300

    (ticket: 10) Added signal processor, db log page to admin panel

--- msizenko_42cc/apps/assignment/admin.py
+++ msizenko_42cc/apps/assignment/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from msizenko_42cc.apps.assignment.models import UserProfile, Contact
+from msizenko_42cc.apps.assignment.models import UserProfile, Contact, DBLog
 
 
 class UserProfileInline(admin.StackedInline):
@@ -14,6 +14,11 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('username', )
     inlines = [UserProfileInline, ContactInline]
     
+class DBLogAdmin(admin.ModelAdmin):
+    list_display = ('date', 'module', 'name', 'action')
+    
+    
 admin.site.unregister(User)    
 admin.site.register(User, UserAdmin)
+admin.site.register(DBLog, DBLogAdmin)
     
